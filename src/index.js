fb007d8c8bb4ceb4cab7af9bb296d687f1e2360c
Seoyeon Lee <67448999+seoyeonleeme@users.noreply.github.com>
2021-07-16 15:11:53 +0900

    back to the list when searching

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import reportWebVitals from './reportWebVitals';
 import Youtube from './service/youtube';
 
 const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
@@ -13,8 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -10,16 +10,16 @@ class Youtube {
         const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=${this.key}`,
             this.getRequestOptions
         );
-        const result_1 = await response.json();
-        return result_1.items;
+        const result = await response.json();
+        return result.items;
     }
 
     async search(query) {
         const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyA_1HfUZXtXrNGAK7oPnEYJIHNu3Rw0IlA&part=snippet&maxResults=26&q=${query}&type=video&key=${this.key}`,
             this.getRequestOptions);
-        const result_1 = await response.json();
-        return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
+        const result = await response.json();
+        return result.items.map(item => ({ ...item, id: item.id.videoId }));
     }
 }
 
-export default Youtube;
+export default Youtube;  
